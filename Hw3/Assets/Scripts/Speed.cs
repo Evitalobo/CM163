a17a5374f096776c243b1faa6bcc92c9f220c6cb
Evita Lobo <elobo@ucsc.edu>
2019-05-27 12:31:43 -0700

    HW 3- minus the build

---
+++ Hw3/Assets/Scripts/Speed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+    Renderer speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = GetComponent<Renderer>();
+
+
+        speed.material.shader = Shader.Find("Custom/NoiseExplosion");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        speed.material.SetFloat("_DisplacementAmt", Input.mousePosition.x / 500);
+      //  speed.material.SetFloat("_Strength", Input.mousePosition.y / 2000);
+
+        //Debug.Log(Input.mousePosition.y);
+
+
+
+    }
+
+}
